3558923298fe1c5ccd8c85ec2ee2c5d6b55a49a6
yuni <hys990730@gmail.com>
2024-06-06 14:16:14 +0900

    fix: Change App component to default export
    - Changed the export of App component to default
    - Ensured compatibility with import statements in other files
    
    Resolved the import error by making App a default export.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
-function App() {
-  return <div className="App"></div>;
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="game">
+          <div className="game-board"></div>
+          <div className="game-info">
+            <div>{/* status */}</div>
+            <ol>{/* TODO */}</ol>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
