3e335df42b8ece2becfe1b4f96add7ebef24a382
yuni <hys990730@gmail.com>
2024-06-06 16:27:35 +0900

    refactor: Change Square component to default export
    - Updated Square component to be exported as default
    - Ensured compatibility with default import statements
    
    Simplified the import process by making Square a default export.

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-export class Square extends Component {
+export default class Square extends Component {
   render() {
     return <button className="square">{this.props.value}</button>;
   }
