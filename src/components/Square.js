52b79b45803c5e695bd09652e378ebafe393cbde
yuni <hys990730@gmail.com>
2024-06-06 16:43:45 +0900

    feat: Square 컴포넌트에 onClick 이벤트 핸들러 추가
    - Square 컴포넌트에 클릭 이벤트 핸들러 추가
    - 버튼 클릭 시 콘솔에 "click" 메시지 출력
    
    Square 컴포넌트에 클릭 이벤트 처리를 추가하여 사용자 인터랙션을 지원합니다.

--- src/components/Square.js
+++ src/components/Square.js
@@ -2,6 +2,10 @@ import React, { Component } from "react";
 import "./Square.css";
 export default class Square extends Component {
   render() {
-    return <button className="square">{this.props.value}</button>;
+    return (
+      <button className="square" onClick={() => console.log("click")}>
+        {this.props.value}
+      </button>
+    );
   }
 }
