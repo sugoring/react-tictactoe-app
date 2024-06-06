dcd1955cfd2362c9a5ca8ac959ef465bfbf364a0
yuni <hys990730@gmail.com>
2024-06-06 16:33:14 +0900

    feat: App 컴포넌트에 Board 컴포넌트 추가
    - Board 컴포넌트를 임포트하여 사용
    - App 컴포넌트에서 Board 컴포넌트를 렌더링하도록 수정
    - App.css 스타일 추가
    
    App 컴포넌트에 게임 보드를 통합하여 초기 게임 레이아웃을 구성했습니다.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from "react";
+import Board from "./components/Board";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="game">
-          <div className="game-board">game-board</div>
-          <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
+          <div className="game-board">
+            <Board />
           </div>
+          <div className="game-info">game-info</div>
         </div>
       </div>
     );
