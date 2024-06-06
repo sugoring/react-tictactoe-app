0c4b0261f7411797b855d7cf31e2fda02a31099f
yuni <hys990730@gmail.com>
2024-06-06 14:21:04 +0900

    style: Add basic styles to index.css and App.css
    - Added default styles to index.css for global styling
    - Included basic styles in App.css for the App component
    
    Set up initial CSS structure for the application.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
     return (
       <div className="App">
         <div className="game">
-          <div className="game-board"></div>
+          <div className="game-board">game-board</div>
           <div className="game-info">
             <div>{/* status */}</div>
             <ol>{/* TODO */}</ol>
